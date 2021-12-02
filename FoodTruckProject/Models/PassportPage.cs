3578846a173d9bc70abe81c21b8ea8c0cd3d84ce
Kas Barber <kaseanbarber02@gmail.com>
2021-12-02 11:42:52 -0500

    First commit

---
+++ FoodTruckProject/Models/PassportPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodTruckProject.Models
+{
+    public class PassportPage
+    {
+        [Key]
+        public int Id { get; set; }
+       
+        public string BusinessId { get; set; }
+        [ForeignKey("ApplicationUser")]
+        public string UserId { get; set; }
+
+    }
+}
