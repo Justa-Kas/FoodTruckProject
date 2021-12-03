4424719054998c1fb577a2164ddccf84e4c4720e
Yousif <ybeeai@gmail.com>
2021-12-03 09:22:06 -0500

    day 2

--- FoodTruckProject/ClientApp/src/app/food-truck.service.ts
+++ FoodTruckProject/ClientApp/src/app/food-truck.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Secret } from './Secret';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
+
 export class FoodTruckService {
     constructor(private http : HttpClient) {
 
@@ -11,10 +14,11 @@ export class FoodTruckService {
   apiUrl: string = 'https://api.yelp.com/v3/businesses/search?term=food trucks&location='
 
   getCityFoodTrucks(city: string): any {
-    let myHeader = new Headers();
+    let myHeader = new HttpHeaders();
     let key: string = 'Bearer ' + Secret.apiKey;
-    myHeader.append('Authorization', key);
+    //myHeader.append('Authorization', key);
+    myHeader.set('Authorization', key);
 
-    return this.http.get(this.apiUrl + city, null, {Headers: myHeader});
+    return this.http.get(this.apiUrl + city, { "headers": myHeader });
   }
 }
