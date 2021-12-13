6f775a9ccea17669f88a82059fd0f384de4d9a34
Yashashree Majalikar <yashmaja@gmail.com>
2021-12-13 09:58:17 -0500

    allergies working

--- FoodTruckProject/ClientApp/src/app/profile/profile.component.ts
+++ FoodTruckProject/ClientApp/src/app/profile/profile.component.ts
@@ -18,14 +18,18 @@ export class ProfileComponent {
   }
 
   currentProfile: Profile = {} as Profile;
-  allAllergies: string[] = [];
   public userID: string = '';
   public profileExists: boolean = true;
   displayEdit: boolean = false;
+  allAllergies: string[] = [];
+  Allergies: string = '';
+  otherAllergy: string = '';
 
 
   ngOnInit(): void {
     this.checkUserID();
+    this.Allergies = '';
+    console.log(this.allAllergies);
   }
 
   checkUserID(): void {
@@ -42,50 +46,41 @@ export class ProfileComponent {
   }
 
   createNewProfile(form: NgForm): void {
+    if (form.form.value.dairy == true) {
+      this.Allergies += 'Dairy;';
+    }
+    if (form.form.value.eggs == true) {
+      this.Allergies += 'Eggs;';
+    }
+    if (form.form.value.peanuts == true) {
+      this.Allergies += 'Peanuts;';
+    }
+    if (form.form.value.treenuts == true) {
+      this.Allergies += 'Tree Nuts;';
+    }
+    if (form.form.value.shellfish == true) {
+      this.Allergies += 'Shellfish;';
+    }
+    if (form.form.value.gluten == true) {
+      this.Allergies += 'Gluten;';
+    }
+    if (form.form.value.soy == true) {
+      this.Allergies += 'Soy;';
+    }
+
     let Name: string = form.form.value.name;
-    let Allergies: string = form.form.value.allergies;
+    /*    let Allergies: string = form.form.value.allergies;*/
+    this.otherAllergy = form.form.value.otherAllergies;
+    this.Allergies += this.otherAllergy;
     let Diet: string = form.form.value.diet;
     let FoodieRating: number = form.form.value.foodierating;
-    let FavFood: string = form.form.value.favfood;
-
-    this.profileservice.createProfile(Name, Allergies, Diet, FoodieRating, FavFood).subscribe((response: any) => {
-      console.log(response);
-      this.routing.navigate(['/user-profile']);
-      this.checkUserID();
-    });
-
-  }
-
-  displayProfile(): void {
-    this.profileservice.getProfile().subscribe((response: any) => {
-      this.currentProfile = response;
-      console.log(response);
-    })
-  }
-
-  resetForm(): void {
-    this.displayEdit = !this.displayEdit;
-}
-
-  updateUserProfile(form: NgForm): void {
-    console.log('method working')
-    //let Allergies: string = '';
-    //if (form.form.value.dairy == true) {
-    //  Allergies += 'Dairy;';
-    //  console.log(Allergies);
-    //}
-    let Name: string = form.form.value.name;
-    let Allergies: string = form.form.value.allergies;
-    let Diet: string = form.form.value.diet;
-    let FoodieRating: number = form.form.value.foodierating;
-    console.log(FoodieRating);
     let FavFood: string = form.form.value.favfood;
 
     if (Name == "") {
       Name = this.currentProfile.name;
     }
-    if (Allergies == "") {
-      Allergies = this.currentProfile.allergies;
+    if (this.Allergies == "") {
+      this.Allergies = this.currentProfile.allergies;
     }
     if (Diet == "") {
       Diet = this.currentProfile.diet;
@@ -97,11 +92,88 @@ export class ProfileComponent {
       FavFood = this.currentProfile.faveFood;
     }
 
-    /*this.allAllergies = this.currentProfile.allergies.split(';');*/
 
-    this.profileservice.updateProfile(Name, Allergies, Diet, FoodieRating, FavFood).subscribe((response: any) => {
+
+    this.profileservice.createProfile(Name, this.Allergies, Diet, FoodieRating, FavFood).subscribe((response: any) => {
+      console.log(response);
+      this.currentProfile = response;
+      this.allAllergies = this.Allergies.split(';');
+      console.log('split allergies')
+      console.log(this.allAllergies);
+      this.routing.navigate(['/user-profile']);
+      this.checkUserID();
+    });
+
+  }
+
+  displayProfile(): void {
+    this.profileservice.getProfile().subscribe((response: any) => {
+      this.currentProfile = response;
+      console.log(response);
+      this.allAllergies = this.currentProfile.allergies.split(';');
+    })
+  }
+
+  resetForm(): void {
+    this.displayEdit = !this.displayEdit;
+}
+
+  updateUserProfile(form: NgForm): void {
+    console.log('method working')
+
+    if (form.form.value.dairy == true) {
+      this.Allergies += 'Dairy;';
+    }
+    if (form.form.value.eggs == true) {
+      this.Allergies += 'Eggs;';
+    }
+    if (form.form.value.peanuts == true) {
+      this.Allergies += 'Peanuts;';
+    }
+    if (form.form.value.treenuts == true) {
+      this.Allergies += 'Tree Nuts;';
+    }
+    if (form.form.value.shellfish == true) {
+      this.Allergies += 'Shellfish;';
+    }
+    if (form.form.value.gluten == true) {
+      this.Allergies += 'Gluten;';
+    }
+    if (form.form.value.soy == true) {
+      this.Allergies += 'Soy;';
+    }
+
+    let Name: string = form.form.value.name;
+    /*    let Allergies: string = form.form.value.allergies;*/
+    this.otherAllergy = form.form.value.otherAllergies;
+    this.Allergies += this.otherAllergy;
+    let Diet: string = form.form.value.diet;
+    let FoodieRating: number = form.form.value.foodierating;
+    console.log(FoodieRating);
+    let FavFood: string = form.form.value.favfood;
+
+    if (Name == "") {
+      Name = this.currentProfile.name;
+    }
+    if (this.Allergies == "") {
+      this.Allergies = this.currentProfile.allergies;
+    }
+    if (Diet == "") {
+      Diet = this.currentProfile.diet;
+    }
+    if (!FoodieRating) {
+      FoodieRating = this.currentProfile.foodieRating;
+    }
+    if (FavFood == "") {
+      FavFood = this.currentProfile.faveFood;
+    }
+
+    this.profileservice.updateProfile(Name, this.Allergies, Diet, FoodieRating, FavFood).subscribe((response: any) => {
       console.log('update working');
       this.currentProfile = response;
+      this.allAllergies = this.Allergies.split(';');
+      console.log('split allergies')
+      console.log(this.allAllergies);
       console.log(response);
       this.displayEdit = false;
       //this.routing.navigate(['/user-profile']);
